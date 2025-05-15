c06ec4dfb30eec7afac9e136c1510360919f2e3b
Dusan Vuckovic <dv@zammad.com>
2025-05-15 14:28:44 +0100

    feat: Add translatable header text on top of all pages.

---
+++ .vitepress/theme/VPHeader.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { useData } from 'vitepress'
+
+const { theme } = useData()
+</script>
+
+<template>
+  <div v-if="theme.header?.text" class="VPHeader">
+    <a v-if="theme.header.link" :href="theme.header.link" target="_blank">
+      {{ theme.header.text }} 🔗
+    </a>
+    <span v-else>
+      {{ theme.header.text }}
+    </span>
+  </div>
+</template>
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -5,12 +5,14 @@ import DefaultTheme from 'vitepress/theme'
 import './style.css'
 import { enhanceAppWithTabs } from 'vitepress-plugin-tabs/client'
 import VPGlossary from './VPGlossary.vue'
+import VPHeader from './VPHeader.vue'
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
+      'layout-top': () => h(VPHeader),
     })
   },
   enhanceApp({ app, router, siteData }) {
