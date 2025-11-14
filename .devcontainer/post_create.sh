367fd75b104fbe85521fa44b36aa903e004607c4
Dusan Vuckovic <dv@zammad.com>
2025-11-14 08:52:49 +0000

    chore: Improve devcontainer experience with VitePress servers and isolated volumes.

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 
+mkdir -p node_modules
+sudo chown -R vscode node_modules
 CI=true pnpm install --frozen-lockfile
 CI=true pnpm screenshots:install
-pnpm --dir ./.screenshots exec cypress install
+mkdir -p .screenshots/node_modules
+sudo chown -R vscode .screenshots/node_modules
+CI=true pnpm --dir ./.screenshots exec cypress install
