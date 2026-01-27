ec4894b5423a5c266272f551454ebc6b2d7aadd5
Martin Gruner <mg@zammad.com>
2026-01-27 07:39:20 +0100

    Update devcontainer to Debian 13

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -3,7 +3,7 @@
 mkdir -p node_modules
 sudo chown -R vscode node_modules
 CI=true pnpm install --frozen-lockfile
-CI=true pnpm screenshots:install
 mkdir -p .screenshots/node_modules
 sudo chown -R vscode .screenshots/node_modules
+CI=true pnpm screenshots:install
 CI=true pnpm --dir ./.screenshots exec cypress install
